4eda31fc0c1059c8c9f731ee8a55d1f017f5cf3b
T.J. Purtell <tpurtell@stanford.edu>
2012-06-10 17:25:27 -0700

    type of obj id

--- src/main/java/mobisocial/cocoon/model/Nearby.java
+++ src/main/java/mobisocial/cocoon/model/Nearby.java
@@ -4,13 +4,14 @@ import java.util.HashSet;
 
 import net.vz.mongodb.jackson.Id;
 import net.vz.mongodb.jackson.MongoCollection;
+import net.vz.mongodb.jackson.ObjectId;
 
 @MongoCollection(name = Nearby.COLLECTION)
 public class Nearby {
 	public static final String COLLECTION = "nearby";
 	
 	@Id 
-	public String _id;
+	public ObjectId _id;
 	
 	public HashSet<String> buckets;
 	public long expiration;
