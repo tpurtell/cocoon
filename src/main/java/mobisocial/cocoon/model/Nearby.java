9cf5b0ba0cf022929c8af8b79fd4c29cf3b96c7f
T.J. Purtell <tpurtell@stanford.edu>
2012-06-10 17:32:41 -0700

    Revert "type of obj id"
    This reverts commit 4eda31fc0c1059c8c9f731ee8a55d1f017f5cf3b.

--- src/main/java/mobisocial/cocoon/model/Nearby.java
+++ src/main/java/mobisocial/cocoon/model/Nearby.java
@@ -4,14 +4,13 @@ import java.util.HashSet;
 
 import net.vz.mongodb.jackson.Id;
 import net.vz.mongodb.jackson.MongoCollection;
-import net.vz.mongodb.jackson.ObjectId;
 
 @MongoCollection(name = Nearby.COLLECTION)
 public class Nearby {
 	public static final String COLLECTION = "nearby";
 	
 	@Id 
-	public ObjectId _id;
+	public String _id;
 	
 	public HashSet<String> buckets;
 	public long expiration;
